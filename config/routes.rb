f6423323cdc7bfcf3e4be1f0ee5b7cf1fe4498a5
myztajay <myztajay@hotmail.com>
2016-12-23 12:21:33 -0500

    Fixed Routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :user
+  root 'articles#index'
 
   resources :articles
   resources :comments
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
